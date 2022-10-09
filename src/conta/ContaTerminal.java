8ff1ecdab3cce5f1d66ad26a9bc4a107dca0e53f
Ricardo <ricardomr8@hotmail.com>
2022-10-09 20:56:25 -0300

    Initial commit

---
+++ src/conta/ContaTerminal.java
@@ -0,0 +1,54 @@
+package conta;
+
+public class ContaTerminal {
+    int numero;
+    String agencia;
+    String nome;
+    double saldo;
+
+    public ContaTerminal(int numero, String agencia, String nome, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", nome, agencia, numero, saldo);
+    }
+}
+
+
---
+++ src/conta/Main.java
@@ -0,0 +1,32 @@
+package conta;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int numero = 0;
+        String agencia = "";
+        String cliente = "";
+        double saldo = 0.00;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da Conta: ");
+        try {
+            numero = Integer.parseInt(in.nextLine());
+        } catch (Exception e) {
+            System.out.println("Não é possível converter");
+        }
+        System.out.println("Por favor, digite o número da agência: ");
+        agencia = in.nextLine();
+        System.out.println("Por favor, digite seu Nome: ");
+        cliente = in.nextLine();
+        System.out.println("Por favor, digite seu saldo (ex: 1500.00) ");
+        try {
+            saldo = Double.parseDouble(in.nextLine());
+        } catch (Exception e) {
+            System.out.println("Não é possível converter, utilize ponto");
+        }
+        ContaTerminal ct = new ContaTerminal(numero, agencia, cliente, saldo);
+        System.out.println(ct);
+    }
+}
